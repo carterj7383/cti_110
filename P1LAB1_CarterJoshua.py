b604ee2d3c62ff86024a5744f392d07edf6070d6
carterj7383 <156561727+carterj7383@users.noreply.github.com>
2024-02-14 10:22:08 -0500

    Add files via upload

---
+++ P1LAB1_CarterJoshua.py
@@ -0,0 +1,13 @@
+#Joshua Carter
+#02/12/2024
+#P1LAB1
+#Using input and output
+
+#Get input from user
+first_name = input ("Enter your first name: ")
+
+#Print first name with greeting
+print ("Hey", first_name)
+
+#Print welcome message
+print ("Welcome to zyBooks!")
